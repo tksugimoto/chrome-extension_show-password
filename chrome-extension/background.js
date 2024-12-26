d5aef4e92c42aa2f81cf1027863bd6628ee33660
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2024-12-26 20:04:45 +0900

    feat: manifest v3対応

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -14,9 +14,16 @@ chrome.runtime.onStartup.addListener(createContextMenus);
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
 	if (info.menuItemId === ID_SHOW_PASSWORD) {
-		chrome.tabs.executeScript(tab.id, {
-			frameId: info.frameId,
-			file: 'show_password.js',
+		chrome.scripting.executeScript({
+			target: {
+				tabId: tab.id,
+				frameIds: [
+					info.frameId,
+				],
+			},
+			files: [
+				'show_password.js',
+			],
 		});
 	}
 });
